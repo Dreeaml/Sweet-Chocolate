f359b9c5085e7388eca84bc948dccb4a195f112b
Ubuntu <ubuntu@ip-172-31-80-42.ec2.internal>
2019-10-31 07:50:13 +0000

    improve 'add_recipe' option and edit recipe

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+

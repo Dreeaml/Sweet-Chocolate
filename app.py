9f2c732e6911c34d4bcd7e4e75a168cf6e62f89e
Ubuntu <ubuntu@ip-172-31-80-42.ec2.internal>
2019-11-28 10:36:25 +0000

    create editcategory.html file, include functionalities, improve connection among templates and app.py

--- app.py
+++ app.py
@@ -1,20 +1,94 @@
 import os
-from flask import Flask
-#from flask_pymongo import PyMongo
-#from bson.objectid import ObjectId
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-#app.config["MONGO_DBNAME"] = 'sweet_chocolate'
-#app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_DBNAME"] = 'sweet_chocolate'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return "hello world"
+@app.route('/get_recipes') #home with all recipes
+def get_recipes():
+    return render_template("recipes.html", task=mongo.db.recipe.find())
 
-#commands to run the funtionalities
+@app.route('/add_recipe') #create new recipe
+def add_recipe():
+    return render_template("add_recipe", task=mongo.db.categories.find())
+    
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes =  mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+    
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipe.find_one({"_id": ObjectId(recipe_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', task=the_recipe,
+                           categories=all_categories)
+                           
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipe = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'title':request.form.get('title'),
+        'category_name':request.form.get('category_name'),
+        'description': request.form.get('description'),
+        'cook_time': request.form.get('cook_time'),
+        'prep_time':request.form.get('prep_time')
+        'number_servings':request.form.get('number_servings')
+        'imgUrl':request.form.get('imgUrl')
+        'ingredients':request.form.get('ingredients')
+        'author':request.form.get('author')
+        'origin':request.form.get('origin')
+    })
+    return redirect(url_for('get_recipes'))
+
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipe.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_recipes'))
+
+#categories create, delete, edit, update and insert.
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+                           categories=mongo.db.categories.find())
+                           
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
 
 
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
